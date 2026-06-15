8831df7acf7bcdc582f43df27f2e11bbd99ae8b1
Dyrina <dylankong.dev@proton.me>
2026-06-15 20:58:56 +0800

    feat: add main.py for cli orchestration

--- week1/main.py
+++ week1/main.py
@@ -1,5 +1,67 @@
+import sys
+from pathlib import Path
+from src.ingestor import ingest_all_mhtml
+from src.processor import process_all_html
+from src.loader import load_all_jsons
+from src.profiler import run_data_profile
+
+SOURCE_DIR = Path("data/0_source")
+BRONZE_DIR = Path("data/1_bronze")
+SILVER_DIR = Path("data/2_silver")
+GOLD_DIR = Path("data/3_gold")
+DB_NAME = "jobs.db"
+
+
+def run_profiler():
+    db_path = GOLD_DIR / DB_NAME
+    run_data_profile(db_path)
+
+
+def run_gold():
+    input_dir = SILVER_DIR
+    output_dir = GOLD_DIR
+    load_all_jsons(input_dir, output_dir)
+
+
+def run_silver():
+    input_dir = BRONZE_DIR
+    output_dir = SILVER_DIR
+    process_all_html(input_dir, output_dir)
+
+
+def run_bronze():
+    input_dir = SOURCE_DIR
+    output_dir = BRONZE_DIR
+    ingest_all_mhtml(input_dir, output_dir)
+
+
 def main():
-    print("Hello from week1!")
+    if len(sys.argv) != 2:
+        print("Error: Invalid number of arguments", file=sys.stderr)
+        print("Usage: uv run python main.py <ingest|process|load|all>", file=sys.stderr)
+        sys.exit(1)
+    command = sys.argv[1]
+    match command:
+        case "ingest":
+            run_bronze()
+        case "process":
+            run_silver()
+        case "load":
+            run_gold()
+        case "profiler":
+            run_profiler()
+        case "all":
+            run_bronze()
+            run_silver()
+            run_gold()
+            run_profiler()
+        case _:
+            print(f"Error: '{command}' is not a valid command", file=sys.stderr)
+            print(
+                "Usage: uv run python main.py <ingest|process|load|all>",
+                file=sys.stderr,
+            )
+            sys.exit(1)
 
 
 if __name__ == "__main__":
